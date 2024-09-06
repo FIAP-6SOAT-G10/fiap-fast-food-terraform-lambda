39c6c87d86abd247b731cdf459253c9f8d81cfcb
Rafael Moura <rafael.mesquita.moura@gmail.com>
2024-09-05 23:36:06 -0300

    feat: Seggregate the API Gateway code from AWS Lambda function code

--- data.tf
+++ data.tf
@@ -1,15 +1,15 @@
-data "archive_file" "handler" {
+data "archive_file" "login" {
   type = "zip"
 
-  source_dir  = "${path.module}/handler"
-  output_path = "${path.module}/handler.zip"
+  source_dir  = "${path.module}/login"
+  output_path = "${path.module}/login.zip"
 }
 
-resource "aws_s3_object" "lambda_handler" {
-  bucket = aws_s3_bucket.lambda_bucket.id
+resource "aws_s3_object" "lambda_login" {
+  bucket = aws_s3_bucket.login_bucket.id
 
-  key    = "handler.zip"
-  source = data.archive_file.handler.output_path
+  key    = "login.zip"
+  source = data.archive_file.login.output_path
 
-  etag = filemd5(data.archive_file.handler.output_path)
+  etag = filemd5(data.archive_file.login.output_path)
 }
